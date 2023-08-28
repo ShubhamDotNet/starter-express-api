63ca8928960a881446fab5b45786718d1dfcaaa3
ShubhamDotNet <141329573+ShubhamDotNet@users.noreply.github.com>
2023-08-28 09:44:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
+app.get('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+
+app.get('/users/user-list', (req, res) => {
+    res.send('You do not have any employee yet!')
+})
+
 app.listen(process.env.PORT || 3000)
